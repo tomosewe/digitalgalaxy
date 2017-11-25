8e30c36a2325ff0114b41b3afa2741109e1baf07
TOMOS EWE <tomosewe@gmail.com>
2017-11-25 20:43:08 +1300

    create separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Boardgames from './Boardgames';
 
 class App extends Component {
   render() {
@@ -10,10 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>woot</p>
+        <Boardgames/>
       </div>
     );
   }
